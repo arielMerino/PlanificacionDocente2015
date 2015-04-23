752e8c42ad5f61150c13fc1fad27e0438c7e49cb
root <root@JANO>
2015-04-22 23:52:04 -0300

    Base del proyecto

---
+++ planificacion-docente-ejb/target/generated-sources/annotations/entities/pruebaEntity_.java
@@ -0,0 +1,16 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(pruebaEntity.class)
+public abstract class pruebaEntity_ {
+
+	public static volatile SingularAttribute<pruebaEntity, Long> id;
+	public static volatile SingularAttribute<pruebaEntity, String> nombre;
+	public static volatile SingularAttribute<pruebaEntity, Integer> at1;
+
+}
+
